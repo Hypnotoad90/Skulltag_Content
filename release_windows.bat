6432c5ff209d22b03a4cf37c7cf95a6ab5dba1a4
Sean Baggaley <sean@csnxs.uk>
2022-09-04 22:11:16 +0100

    Update 7z and use highest compression

--- release_windows.bat
+++ release_windows.bat
@@ -1,3 +1,3 @@
 @echo off
-7z a -tzip ./Skulltag_Content-dev.pk3 ./skulltag_content/* -xr!*.dbs -xr!*.bak -xr!*.backup1 -xr!*.backup2 -xr!*.backup3
+7za a -tzip -mx=9 ./Skulltag_Content-dev.pk3 ./skulltag_content/* -xr!*.dbs -xr!*.bak -xr!*.backup1 -xr!*.backup2 -xr!*.backup3
 pause 
